ea74d91cb0279691fde6e52011d5b73cbb495307
Viktor Berczeli <86021488+syzgod@users.noreply.github.com>
2023-04-17 13:34:29 +0200

    Add components

---
+++ src/components/TermsAndConditions.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+
+const TermsAndConditions = () => {
+  return (
+    <div>
+      <h1>Terms and Conditions</h1>
+      <ol>
+        <li>
+          <h2>Scope</h2>
+          <p>
+            These terms and conditions govern the contractual relationship
+            between the customer and the seller in connection with all products
+            offered in the online shop. The customer acknowledges these terms
+            and conditions when placing an order.
+          </p>
+        </li>
+        <li>
+          <h2>Conclusion of contract</h2>{' '}
+          <p>
+            The presentation of products in the online shop does not constitute
+            a binding offer by the seller. By clicking the "Buy Now" button, the
+            customer places a binding order for the products in the shopping
+            cart. The seller confirms receipt of the order by email. The
+            contract is concluded when the seller sends a confirmation of
+            acceptance to the customer by email.
+          </p>
+        </li>
+        <li>
+          <h2>Payment</h2>{' '}
+          <p>
+            The prices quoted in the online shop are final prices including VAT.
+            Payment can be made by credit card, PayPal, or other payment methods
+            offered in the online shop. The seller reserves the right to exclude
+            certain payment methods for individual orders.
+          </p>
+        </li>
+        <li>
+          <h2>Delivery</h2>{' '}
+          <p>
+            The seller delivers to the delivery address specified by the
+            customer. The delivery time depends on the availability of the
+            products and the shipping method selected by the customer. The
+            seller will inform the customer of the expected delivery time in the
+            order confirmation. If the delivery time is exceeded, the customer
+            has the right to withdraw from the contract.
+          </p>
+        </li>
+        <li>
+          <h2>Right of withdrawal</h2>{' '}
+          <p>
+            The customer has the right to withdraw from the contract within 14
+            days without giving any reason. The withdrawal period begins on the
+            day on which the customer or a third party designated by the
+            customer, who is not the carrier, takes possession of the goods. To
+            exercise the right of withdrawal, the customer must inform the
+            seller by means of a clear statement. The customer may use the model
+            withdrawal form provided by the seller for this purpose, but this is
+            not mandatory.
+          </p>
+        </li>
+        <li>
+          <h2>Warranty</h2>{' '}
+          <p>
+            The statutory warranty rights apply to all products offered in the
+            online shop. If the delivered goods are defective, the customer has
+            the right to demand subsequent performance, i.e. either repair or
+            replacement. If subsequent performance fails, the customer has the
+            right to withdraw from the contract or to reduce the purchase price.
+          </p>
+        </li>
+        <li>
+          <h2>Liability</h2>{' '}
+          <p>
+            The seller is liable for damages caused by intent or gross
+            negligence. In case of slight negligence, the seller is liable only
+            if an obligation is violated, the fulfillment of which is essential
+            for the proper execution of the contract and on whose compliance the
+            customer can regularly rely.
+          </p>
+        </li>
+        <li>
+          <h2>Data protection</h2>{' '}
+          <p>
+            The seller collects and uses the personal data of the customer in
+            accordance with the applicable data protection regulations. The data
+            protection declaration of the seller, which can be accessed on the
+            website, provides further information.
+          </p>
+        </li>
+        <li>
+          <h2>Applicable law and jurisdiction</h2>{' '}
+          <p>
+            The law of the Federal Republic of Germany applies to all legal
+            relationships between the seller and the customer. The exclusive
+            place of jurisdiction for all disputes arising from or in connection
+            with the contract is the registered office of the seller, provided
+            that the customer is a merchant or a legal entity under public law.
+          </p>
+        </li>
+      </ol>
+    </div>
+  );
+};
+
+export default TermsAndConditions;
---
+++ src/components/TopBanner/TopBanner.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const TopBanner = () => {
+  return (
+    <div>
+      <li>
+        <a href='#'>Fast delivery</a>
+      </li>
+      <li>
+        <a href='#'>Free delivery above €50</a>
+      </li>
+      <li>
+        <a href='#'>Click & Collect</a>
+      </li>
+    </div>
+  );
+};
+
+export default TopBanner;
