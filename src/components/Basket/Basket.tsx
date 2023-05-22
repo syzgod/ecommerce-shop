3b2444e0b167fc43b01d627512641c0d21855aae
syzgod <viktor.berczeli@gmail.com>
2023-05-22 12:17:41 +0200

    Add Basket basics

---
+++ src/components/Basket/Basket.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Basket = () => {
+  return (
+    <div>
+      <img src='' alt='' />
+      <p>Category</p>
+      <p>Description</p>
+      <p>Price</p>
+      <span>-</span>
+      <div>Amount</div>
+      <span>+</span>
+      <p>Promotional Code</p>
+      <button>Apply Code</button>
+      <button>Change Delivery Option</button>
+      <div>Delivery Cost</div>
+      <div>Total Cost</div>
+      <button>Continue Shopping</button>
+      <button>Checkout</button>
+    </div>
+  );
+};
+
+export default Basket;
