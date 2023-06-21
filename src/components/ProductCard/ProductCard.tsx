d43e836e79060e8eeb70b3e72f920fefdc46dd71
syzgod <viktor.berczeli@gmail.com>
2023-06-21 12:11:36 +0200

    Add Sale ribbon and sale price

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -6,6 +6,7 @@ const ProductCard = ({ filteredProducts }: any) => {
     <div className='product-card' key={product.id}>
       <div className='product-info'>
         <h1>{product.category}</h1>
+        <div className='ribbon-sale'>Sale</div>
         <div className='product-image__container'>
           <img className='product-image' src={`${product.image}`} alt='' />
         </div>
@@ -14,6 +15,7 @@ const ProductCard = ({ filteredProducts }: any) => {
       <div className='card-bottom'>
         <StarRating rating={product.rating.rate} count={product.rating.count} />
         <p>${product.price}</p>
+        <div>${(product.price * 0.8).toFixed(2)}</div>
         <div className='option-buttons'>
           <ProductButton option={'See more'} />
           <ProductButton option={'Add to Basket'} />
