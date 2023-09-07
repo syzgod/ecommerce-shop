61683f4aeb5d9a83e7b07e680c4df362f0006f7e
syzgod <viktor.berczeli@gmail.com>
2023-09-07 22:52:35 +0200

    Finetune Login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,13 +1,32 @@
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
+import logo from '../../assets/All-I-Buy.png';
+import { useState } from 'react';
 
 interface LoginProps {
   setIsLoggedIn: (value: boolean) => void;
 }
 
 const Login = ({ setIsLoggedIn }: LoginProps) => {
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+
   const handleLogin = () => {
     setIsLoggedIn(true);
+  };
+
+  const handleTester = () => {
+    setUserEmail('test@test.com');
+    setUserPassword('test');
+    setIsLoggedIn(true);
+  };
+
+  const handleEmail = (e: any) => {
+    setUserEmail(e.target.value);
+  };
+
+  const handlePassword = (e: any) => {
+    setUserPassword(e.target.value);
   };
 
   return (
@@ -17,22 +36,47 @@ const Login = ({ setIsLoggedIn }: LoginProps) => {
       animate={{ opacity: 1, scale: 1 }}
       transition={{ duration: 1 }}
     >
-      <form action='submit'>
-        <label htmlFor='email'>E-mail</label>
-        <input type='text' name='' id='email' value='test@test.com' />
-        <label htmlFor='password'>Password</label>
-        <input type='text' name='' id='password' value='test' />
-        <div className='form-buttons'>
-          <Link to='/' onClick={handleLogin}>
-            Login
-          </Link>
-          <Link to='/register'>Register</Link>
-        </div>
-        <div className='helper-links'>
-          <a href='#'>Forgot password?</a>
-          <a href='#'>Need help?</a>
-        </div>
-      </form>
+      <div className='login-form'>
+        <img src={logo} alt='all-i-buy-logo' />
+        <form action='submit'>
+          <label htmlFor='email'>E-mail</label>
+          <input
+            type='text'
+            name=''
+            id='email'
+            value={userEmail}
+            onChange={handleEmail}
+          />
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            name=''
+            id='password'
+            value={userPassword}
+            onChange={handlePassword}
+          />
+          <div className='form-buttons'>
+            <div className='action-buttons'>
+              <Link to='/' onClick={handleLogin}>
+                Login
+              </Link>
+              <Link to='/register'>Register</Link>
+            </div>
+            <div className='login-type'>
+              <Link to='/' onClick={handleTester}>
+                Tester
+              </Link>
+              <Link to='/' onClick={handleLogin}>
+                Admin
+              </Link>
+            </div>
+          </div>
+          <div className='helper-links'>
+            <a href='#'>Forgot password?</a>
+            <a href='#'>Need help?</a>
+          </div>
+        </form>
+      </div>
     </motion.div>
   );
 };
